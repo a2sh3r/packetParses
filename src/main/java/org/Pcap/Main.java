48b2503ee693162649c743cd375e8ffb3cee0fb9
a2sh3r <61034206+a2sh3r@users.noreply.github.com>
2023-04-06 10:29:31 +0300

    Добавлен метод toString выдающий все параметры пакета

--- src/main/java/org/Pcap/Main.java
+++ src/main/java/org/Pcap/Main.java
@@ -13,22 +13,8 @@ public class Main {
         listener.addListener(packet -> {
             Optional<SvPacket> svPacket = svDecoder.decode(packet);
             if(svPacket.isPresent()){
-                System.out.println("instIa - " + svPacket.get().getDataset().getInstIa());
-                System.out.println("instIb - " + svPacket.get().getDataset().getInstIb());
-                System.out.println("instIc - " + svPacket.get().getDataset().getInstIc());
-                System.out.println("instIn - " + svPacket.get().getDataset().getInstIn());
-                System.out.println("qIa - " + svPacket.get().getDataset().getQIa());
-                System.out.println("qIb - " + svPacket.get().getDataset().getQIb());
-                System.out.println("qIc - " + svPacket.get().getDataset().getQIc());
-                System.out.println("qIn - " + svPacket.get().getDataset().getQIn());
-                System.out.println("instUa - " + svPacket.get().getDataset().getInstUa());
-                System.out.println("instUb - " + svPacket.get().getDataset().getInstUb());
-                System.out.println("instUc - " + svPacket.get().getDataset().getInstUc());
-                System.out.println("instUn - " + svPacket.get().getDataset().getInstUn());
-                System.out.println("qUa - " + svPacket.get().getDataset().getQUa());
-                System.out.println("qUb - " + svPacket.get().getDataset().getQUb());
-                System.out.println("qUc - " + svPacket.get().getDataset().getQUc());
-                System.out.println("qUn - " + svPacket.get().getDataset().getQUn());
+                System.out.println("Answer - \n" + svPacket.get().toString(svPacket.get().getDataset()));
+
             }
 
         });
--- src/main/java/org/Pcap/SvPacket.java
+++ src/main/java/org/Pcap/SvPacket.java
@@ -44,7 +44,29 @@ public class SvPacket {
         private double instUn;
         private int qUn;
 
+    }
 
+    public String toString(Dataset dataset){
+        String answer = "MAC dist: " + macDst + "\nMac source: " + masSrc +"\nPacket type: "+packetType+"\nApp Id: " +
+                Short.toString(appID) + "\nSv Id: "+svId+"\nSample count : "+Integer.toString(smpCount)+"\nConf Ref: "+
+                Integer.toString(confRef)+"\nSample Synch: "+Integer.toString(smpSynch) + "\n Inst IA: " +
+                Double.toString(dataset.getInstIa())+"\n quality IA: " +
+                Integer.toString(dataset.getQIa())+"\n Inst IB: " +
+                Double.toString(dataset.getInstIb())+"\n quality IB: " +
+                Integer.toString(dataset.getQIb())+"\n Inst IC: " +
+                Double.toString(dataset.getInstIc())+"\n quality IC: " +
+                Integer.toString(dataset.getQIc())+"\n Inst IN: " +
+                Double.toString(dataset.getInstIn())+"\n quality IN: " +
+                Integer.toString(dataset.getQIn())+"\n Inst UA: " +
+                Double.toString(dataset.getInstUa())+"\n quality UA: " +
+                Integer.toString(dataset.getQUa())+"\n Inst UB: " +
+                Double.toString(dataset.getInstUb())+"\n quality UB: " +
+                Integer.toString(dataset.getQUb())+"\n Inst UC: " +
+                Double.toString(dataset.getInstUc())+"\n quality UC: " +
+                Integer.toString(dataset.getQUc())+"\n Inst UN: " +
+                Double.toString(dataset.getInstUn())+"\n quality UN: " +
+                Integer.toString(dataset.getQUn());
+        return answer;
     }
 
 }
